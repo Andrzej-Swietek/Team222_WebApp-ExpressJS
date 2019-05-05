d090dd05066110072648d8e5aca155e4d67111b5
Andrzej <mr.andi31@gmail.com>
2019-05-04 23:16:14 -0400

    May4thChangesSubmited

--- main/static/js/history.js
+++
@@ -1,8 +0,0 @@
-
-   $(document).ready(function()
-    {
-        $(window).scroll(function(){
-            $('.header-bg').css("opacity",1-$(window).scrollTop()/700)
-        })
-    })
-                   
