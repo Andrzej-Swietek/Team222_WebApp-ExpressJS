183d5045010572173cacac7a31b4f46325497148
Andrzej <mr.andi31@gmail.com>
2019-05-04 22:54:29 -0400

    meant to work ...

---
+++ main/static/js/history.js
@@ -0,0 +1,8 @@
+
+   $(document).ready(function()
+    {
+        $(window).scroll(function(){
+            $('.header-bg').css("opacity",1-$(window).scrollTop()/700)
+        })
+    })
+                   
